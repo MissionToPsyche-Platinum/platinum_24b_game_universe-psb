45608f374f74fa27947ee87e7f525c103de256a6
subbie7 <168138879+subbie7@users.noreply.github.com>
2025-11-04 11:53:24 -0600

    Added Deal Cards Scripts
    Created scripts to instantiate, shuffle, and deal answer cards. Also added some banter, and fixed punctuation.

---
+++ Psyche Against the Universe version 1.0/Assets/Scripts/AnswerCard.cs
@@ -0,0 +1,10 @@
+// Defines an answer card
+// Still under construction, will be completed when cards are finalized
+// Version 1.0 by Abdur-Rahman Igram
+
+[System.Serializable]
+public class AnswerCard {
+    public string title;
+    //public string text; I don't know if this is neccesary or not, title might be enough
+    // this will be filled in as the cards are developed
+}
---
+++ Psyche Against the Universe version 1.0/Assets/Scripts/PromptCard.cs
@@ -0,0 +1,9 @@
+// Defines a prompt card
+// Still under construction, will be completed when cards are finalized
+// Version 1.0 by Abdur-Rahman Igram
+
+[System.Serializable]
+public class PromptCard {
+    public string prompt;
+    // this will be filled in as the cards are developed
+}
