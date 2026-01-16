98ad008f4f46c83f1db66099b95a15994573d9dc
tb9482 <156216937+tb9482@users.noreply.github.com>
2026-01-16 16:43:06 -0500

    Main update
    Post ASMDEF fix with main menu updates

--- Psyche Against the Universe version 1.0/Assets/Tests/PlayMode/Tests/Mocks/MockCPUBuilder.cs
+++ Psyche Against the Universe version 1.0/Assets/Tests/PlayMode/Tests/Mocks/MockCPUBuilder.cs
@@ -1,5 +1,7 @@
-using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 /// <summary>
 /// Srcipt provides a mockup of the CPU builder to prevent testing crashes during play mode testing
@@ -7,34 +9,46 @@ using System.Collections.Generic;
 public class MockCPUBuilder : ICPUBuilder
 {
     private int counter = 1;
-    private string name = "TestPlayer";
-    private string[] personality = new string[0];
+    //private string name = "TestPlayer";
+    private static readonly string[] Ptrait =
+        { "Funny", "Serious", "Chaotic", "SciFi" };
 
-    public IPlayerCommon Build()
-    {
-        return new MockPlayer(name + counter++);
-
-    }
+    private System.Random rng = new System.Random(); //generate the random personailty matrix
+    public IPlayerCommon Build() => BuildCPUPlayer();
+  
 
     public IPlayerCommon BuildCPUPlayer()
     {
-        return Build();
+        //return Build();
+        string name = "TestPlayer" + counter;
+        var traitmatrix = Ptrait.ToArray();
+        Shuffle(Ptrait);
+
+        return new MockPlayer(name, traitmatrix);
 
     }
 
     public void Reset()
     {
-        counter = 1;
+        //counter = 1;
     }
 
     public void SetCPUname(string name)
     {
-        this.name = name;
+      //not required for testing
     }
 
     public void SetCPUpersonality(string[] personality)
     {
-        this.personality = personality;
+       //not required for testing
+    }
+    private void Shuffle(string[] array)
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
     }
 
 }
@@ -42,35 +56,41 @@ public class MockCPUBuilder : ICPUBuilder
 public class MockPlayer : IPlayerCommon
 {
     public string Avatar_Name { get;  set; }
+    public string[] Personality { get; set; }
+
     string IPlayerCommon.Avatar_Name { get => Avatar_Name; set => Avatar_Name = value; }
     public int score { get ; set ; }
     public bool judge { get ; set ; }
     public List<AnswerCard> Hand { get ; set; }
 
-    public MockPlayer(string name)
+    public MockPlayer(string name, string[] personality)
     {
         Avatar_Name = name;
+        Personality = personality;
     }
+
 
     public void PlayCard(GameLoop gameLoop)
     {
-        
+        //not required for testing
     }
 
     public void PLayCard(GameLoop gameLoop, int Index)
     {
+        //not required for testing
     }
 
     public void DrawCard()
     {
-       
+        //not required for testing
     }
 
     public void PlayCard()
     {
-       
+        //not required for testing
     }
 
+    //not required for testing
     public bool isJudge() => judge;
     
 }
